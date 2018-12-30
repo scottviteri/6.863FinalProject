1c04bdd2f415900270e4a744e0177f20a76cca48
Scott Viteri <scottviteri@gmail.com>
2018-12-30 10:59:59 -0500

    Enforced tense consistency in examples, removed jupyter from shell.nix, added PDF of notebook

--- shell.nix
+++ shell.nix
@@ -3,13 +3,10 @@ stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
-    python
     python27Packages.nltk    
     python27Packages.numpy
     python27Packages.tkinter
-    python27Packages.jupyter
     python27Packages.matplotlib
-    python27Packages.ipywidgets
     pandoc
     texlive.combined.scheme-full
   ];
